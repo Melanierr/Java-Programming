86366a7442fce8b65da5bf0113a5924a760087f1
Musc <97372099+Melanierr@users.noreply.github.com>
2026-07-20 13:44:43 +0700

    nothing

--- Project3_BankManagementSystem/CheckingAccount.java
+++ Project3_BankManagementSystem/CheckingAccount.java
@@ -1,5 +1,9 @@
 public class CheckingAccount extends BankAccount{
+    String accountType = "checkingaccount";
     CheckingAccount(String name, String accountNumber, double balance){
         super(name,accountNumber,balance);
+    }
+    public String getAccountType(){
+        return accountType;
     }
 }
